3f57f24a46ba666e46f4375c38aa70ff38a5e341
김석진 <69078017+eeptmxlsl15@users.noreply.github.com>
2025-01-12 02:15:15 +0900

    feat : target change
    몬스터가 세계수 이외에 닿았을때 목표를 닿은 물체로 하고 그 물체와 떨어지면 다시 세계수로 타겟을 변경

--- Demeter of Hell/Assets/2. Scripts/Seokin/NavAgent2D.cs
+++ Demeter of Hell/Assets/2. Scripts/Seokin/NavAgent2D.cs
@@ -4,6 +4,7 @@ using UnityEngine.AI;
 public class NavAgent2D : MonoBehaviour
 {
     NavMeshAgent agent;
+    public Transform worldTreePosition;
     public Transform target;
 
     private void Awake()
@@ -11,10 +12,25 @@ public class NavAgent2D : MonoBehaviour
         agent = GetComponent<NavMeshAgent>();
         agent.updateRotation = false;
         agent.updateUpAxis = false;
+
+        //시작 시 첫 목표는 세계수
+        target = worldTreePosition;
     }
 
     private void Update()
     {
         agent.SetDestination(target.position);
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        target = collision.transform;
+        
+        Debug.Log("d");
+    }
+
+    private void OnCollisionExit2D(Collision2D collision)
+    {
+        target = worldTreePosition;
     }
 }
