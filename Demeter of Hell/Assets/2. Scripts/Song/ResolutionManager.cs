6b44c7e26c99454db1c2501e2522b9d4b7912d51
SongMonggu <ssg42190@gmail.com>
2025-01-12 19:17:22 +0900

    Resolution function
    해상도 기능 추가 완료(컴퓨터가 지원하는 해상도를 기준으로 1280 이상의 해상도부터 최대 해상도 및 전체화면, 창모드 구현 완료)

--- Demeter of Hell/Assets/2. Scripts/Song/ResolutionManager.cs
+++ Demeter of Hell/Assets/2. Scripts/Song/ResolutionManager.cs
@@ -5,11 +5,11 @@ using TMPro;
 
 public class ResolutionManager : MonoBehaviour
 {
-    FullScreenMode fullScreenMode;
-    public TMP_Dropdown resolutionDropdown;
-    public Toggle fullScreenToggle;
-    List<Resolution> resolutions = new List<Resolution>();
-    public int resolutionNum;
+    FullScreenMode fullScreenMode; // 전체화면 모드 지원 변수
+    public TMP_Dropdown resolutionDropdown; // 드롭다운 오브젝트
+    public Toggle fullScreenToggle;  // 전체화면 모드 체크박스
+    List<Resolution> resolutions = new List<Resolution>(); // 지원 해상도 리스트
+    public int resolutionNum;  // 선택한 해상도 리스트 인덱스 값
 
     void Start()
     {
@@ -17,18 +17,19 @@ public class ResolutionManager : MonoBehaviour
     }
 
     void InitUI()
-    {
+    {   // 해상도가 1280보다 큰 16:9 화변비의 지원 해상도 출력
         for(int i = 0; i< Screen.resolutions.Length; i++)
         {
-            if (Screen.resolutions[i].width % 16 == 0 &&  Screen.resolutions[i].height % 16 == 0)
+            if (Screen.resolutions[i].width >= 1280 && Screen.resolutions[i].width % 16 == 0 &&  Screen.resolutions[i].height % 9 == 0)
             {
                 resolutions.Add(Screen.resolutions[i]);
             }
         }
 
-        resolutionDropdown.options.Clear();
-        int optionNum = 0;
-        foreach(Resolution item in resolutions)
+        resolutionDropdown.options.Clear();  // 드롭다운의 요소 값 초기화
+        resolutionNum = 0;
+
+        foreach(Resolution item in resolutions)  // 지원해주는 해상도를 ooo x ooo으로 표기
         {
             TMP_Dropdown.OptionData option = new TMP_Dropdown.OptionData();
             option.text = item.width + " x " + item.height;
@@ -36,10 +37,10 @@ public class ResolutionManager : MonoBehaviour
         
             if(item.width == Screen.width && item.height == Screen.height)
             {
-                resolutionDropdown.value = optionNum;
-                optionNum++;
+                resolutionDropdown.value = resolutionNum;
+                resolutionNum++;
             }
-            resolutionDropdown.RefreshShownValue();
+            resolutionDropdown.RefreshShownValue(); // 드롭다운에서 나타나는 값 새로고침
 
             fullScreenToggle.isOn = Screen.fullScreenMode.Equals(FullScreenMode.FullScreenWindow) ? true : false;
         }
