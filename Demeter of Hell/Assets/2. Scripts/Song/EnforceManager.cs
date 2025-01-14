dd57cbd98f847aa0ccb110200c6487d3814b4961
SongMonggu <ssg42190@gmail.com>
2025-01-14 17:33:07 +0900

    Enforce System
    강화시스템 제작중

---
+++ Demeter of Hell/Assets/2. Scripts/Song/EnforceManager.cs
@@ -0,0 +1,15 @@
+using UnityEngine;
+
+public class EnforceManager : MonoBehaviour
+{
+    // 캐릭터 레벨업 특성 및 팝업
+    [SerializeField] Transform characterEnforceItemParent; 
+    [SerializeField] GameObject characterEnforceItem; // 나주엥 GameObject는 다른 형태의 스크립트 오브젝트로 대체
+
+    // 식물 레벨업 특성 및 팝업
+    [SerializeField] Transform plantEnforceItemParent; 
+    [SerializeField] GameObject plantEnforceItem;
+
+
+    
+}
